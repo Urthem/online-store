75337b5b43adbe7bbb358fc3c6db05a1847b7ce0
lauwh <lauwh@lauwhs-Air.vtc.hk>
2020-07-08 12:39:06 +0800

    Updates

---
+++ precache-manifest.5751bca877546f02a5b2ac1f698ddf7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682466a68c63f849bc3ead843c0f1923",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "c1334495646526d38fc0",
+    "url": "/online-store/static/css/main.e888507a.chunk.css"
+  },
+  {
+    "revision": "b5d1335e40e84644c58e",
+    "url": "/online-store/static/js/2.c49c6f7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-store/static/js/2.c49c6f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1334495646526d38fc0",
+    "url": "/online-store/static/js/main.299ee26c.chunk.js"
+  },
+  {
+    "revision": "307fdab4860557be91db",
+    "url": "/online-store/static/js/runtime-main.d0ac6c5e.js"
+  }
+]);
